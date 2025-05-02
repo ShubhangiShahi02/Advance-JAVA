534d75293da4371b693d5cd7fb67690eb0ca89b7
ShubhangiShahi02 <spardhashahi@gmail.com>
2025-05-02 13:51:40 +0530

    Servlet

---
+++ Servlet/SubmitServlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/submit")
+public class SubmitServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        out.println("<html><body>");
+        out.println("<h2>Thank you for registering!</h2>");
+        out.println("<p>Name: " + name + "</p>");
+        out.println("<p>Email: " + email + "</p>");
+        out.println("</body></html>");
+    }
+}
